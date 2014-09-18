fbfcf3349dbeefdd42bd25ca7d623e1d8450fa6a
lyjordan <lyjordan@gmail.com>
2014-09-18 19:52:13 +0800

    update raw cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
 
 default_scope {order('name DESC')}
+has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
 #attr_accessible :description, :image_url, :price, :name, :discount, :category
   validates :name, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -11,4 +13,13 @@ default_scope {order('name DESC')}
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
   validates :discount, numericality: {less_than_or_equal_to: 1}
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
   end
--- config/initializers/assets.rb
+++
@@ -1,1 +0,0 @@
-Rails.application.config.assets.precompile += %w( bootstrap.min.css )
